654530d0625cc83a9a5f963f68f834a94ba8a27f
keeran2488 <49069135+keeran2488@users.noreply.github.com>
2020-02-19 14:12:09 +0545

    login page/ home page ko boilerplate

---
+++ lib/auth_handler.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AuthHandler extends StatefulWidget {
+  @override
+  _AuthHandlerState createState() => _AuthHandlerState();
+}
+
+class _AuthHandlerState extends State<AuthHandler> {
+  String _email, _password;
+  final GlobalKey<FormState> _loginFormKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login form"),
+      ),
+      body: Form(
+        key: _loginFormKey,
+        child: 
+        )
+    );
+  }
+}
